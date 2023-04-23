ab21a0e888a8057ef0811a7ba20f84cd4d8cb50c
Bruce Mayenschein <brucedmayenschein@gmail.com>
2023-04-22 22:21:54 -0400

    addming MVC structure

--- server.js
+++ server.js
@@ -1,65 +1,63 @@
 const express = require("express");
 const app = express();
-const MongoClient = require("mongodb").MongoClient;
-const PORT = 8101;
-require('dotenv').config();
+const connectDB = require('./config/database')
+const homeRoutes = require('./routes/home')
+const todoRoutes = require('./routes/todos')
 
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'Todo_App'
+require('dotenv').config({path: './config/.env'})
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(async client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName);
-    })
+connectDB();
+
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use('/', homeRoutes);
+app.use('/todos', todoRoutes);
 
-app.get('/', async (req, res) => {
-    const todoItems = await db.collection('Item_List').find().toArray();
-    const itemsLeft = await db.collection('Item_List').countDocuments({completed: false});
 
-    res.render('index.ejs', { items: todoItems, left: itemsLeft})
-})
+// app.get('/', async (req, res) => {
+//     const todoItems = await db.collection('Item_List').find().toArray();
+//     const itemsLeft = await db.collection('Item_List').countDocuments({completed: false});
 
-app.post('/addTodo', async(req, res) => {
-    db.collection('Item_List').insertOne({task: req.body.todoItem, completed: false})
-    .then(result => {
-        console.log('Todo Added');
-        res.redirect('/');
-    })
-    .catch(error => console.log(error))
-})
+//     res.render('index.ejs', { items: todoItems, left: itemsLeft})
+// })
 
-app.put('/setComplete', async(req,res)=> {
-    const item = await req.body.item;
+// app.post('/addTodo', async(req, res) => {
+//     db.collection('Item_List').insertOne({task: req.body.todoItem, completed: false})
+//     .then(result => {
+//         console.log('Todo Added');
+//         res.redirect('/');
+//     })
+//     .catch(error => console.log(error))
+// })
+
+// app.put('/setComplete', async(req,res)=> {
+//     const item = await req.body.item;
     
   
-    db.collection('Item_List').updateOne(
-        { "task" : item },
-        [
-            {
-                $set: {
-                    'completed': {
-                        $cond: {
-                            if: {
-                                $eq: ['$completed', false]
-                            },
-                            then: true,
-                            else: false
-                        }
-                    }
-                }
-            }
-        ]
+//     db.collection('Item_List').updateOne(
+//         { "task" : item },
+//         [
+//             {
+//                 $set: {
+//                     'completed': {
+//                         $cond: {
+//                             if: {
+//                                 $eq: ['$completed', false]
+//                             },
+//                             then: true,
+//                             else: false
+//                         }
+//                     }
+//                 }
+//             }
+//         ]
         
-    )
+//     )
     // db.collection('Item_List').findOne({task: item})
     // .then(result => {
     //     console.log(result.completed)
@@ -86,9 +84,9 @@ app.put('/setComplete', async(req,res)=> {
     //     }
     //     console.log(result);
     // })
-    res.json('Task has been updated');
-})
+//     res.json('Task has been updated');
+// })
 
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server running on port ${process.env.PORT}`)
 });
